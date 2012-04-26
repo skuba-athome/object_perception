3a7361d6965948b6f1db2dee24afa4cab59931aa
Khakhana  Thimachai <khakhana@live.com>
2012-04-27 00:00:00 +0700

    Modify objects

---
+++ src/ece.cpp
@@ -0,0 +1,184 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+// PCL specific includes
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/features/vfh.h>
+
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+ros::Publisher pub;
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal> ());
+	pcl::fromROSMsg(*input , *cloud);
+
+	std::cout << cloud->height << " " << cloud->width << std::endl;
+
+	// Create the filtering object
+	pcl::PassThrough<pcl::PointXYZ> pass;
+	pass.setInputCloud (cloud);
+	pass.setFilterFieldName ("z");
+	pass.setFilterLimits (0.4, 2.0);
+	pass.filter (*cloud);
+
+
+	std::cout << "PointCloud before filtering has: " << cloud->points.size () << " data points." << std::endl; //*
+
+
+	// Create the filtering object: downsample the dataset using a leaf size of 1cm
+	pcl::VoxelGrid<pcl::PointXYZ> vg;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+	vg.setInputCloud (cloud);
+	vg.setLeafSize (0.005f, 0.005f, 0.005f);
+	vg.filter (*cloud_filtered);
+	std::cout << "PointCloud after filtering has: " << cloud_filtered->points.size ()  << " data points." << std::endl; //*
+
+
+	// Create the segmentation object for the planar model and set all the parameters
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+	pcl::PCDWriter writer;
+	seg.setOptimizeCoefficients (true);
+	seg.setModelType (pcl::SACMODEL_PLANE);
+	seg.setMethodType (pcl::SAC_RANSAC);
+	seg.setMaxIterations (100);
+	seg.setDistanceThreshold (0.01);
+
+	int i=0, nr_points = (int) cloud_filtered->points.size ();
+	while (cloud_filtered->points.size () > 0.3 * nr_points)
+	{
+		// Segment the largest planar component from the remaining cloud
+		seg.setInputCloud (cloud_filtered);
+	    seg.segment (*inliers, *coefficients);
+	    if (inliers->indices.size () == 0)
+	    {
+	    	std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+	    	break;
+	    }
+
+	    // Extract the planar inliers from the input cloud
+	    pcl::ExtractIndices<pcl::PointXYZ> extract;
+	    extract.setInputCloud (cloud_filtered);
+	    extract.setIndices (inliers);
+	    extract.setNegative (false);
+
+	    // Write the planar inliers to disk
+	    extract.filter (*cloud_plane);
+	    std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
+
+	    // Remove the planar inliers, extract the rest
+	    extract.setNegative (true);
+	    extract.filter (*cloud_f);
+	    cloud_filtered = cloud_f;
+	}
+
+	  // Creating the KdTree object for the search method of the extraction
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+	tree->setInputCloud (cloud_filtered);
+
+	std::vector<pcl::PointIndices> cluster_indices;
+	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+	ec.setClusterTolerance (0.02); // 2cm
+	ec.setMinClusterSize (100);
+	ec.setMaxClusterSize (25000);
+	ec.setSearchMethod (tree);
+	ec.setInputCloud (cloud_filtered);
+	ec.extract (cluster_indices);
+
+	int j = 0;
+	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+	{
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+
+		for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
+			cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
+
+		cloud_cluster->width = cloud_cluster->points.size ();
+		cloud_cluster->height = 1;
+		cloud_cluster->is_dense = true;
+
+		std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+
+	    std::stringstream ss;
+		ss << "cloud_cluster_" << j << ".pcd";
+		writer.write<pcl::PointXYZ> (ss.str (), *cloud_cluster, false); //
+		j++;
+
+
+		//============================== vfh =====================================
+
+		// normal estimate
+		pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+		ne.setInputCloud (cloud_cluster);
+		// Use all neighbors in a sphere of radius 3cm
+		ne.setRadiusSearch (0.03);
+		// Compute the features
+		ne.compute (*normals);
+
+		// Create the VFH estimation class, and pass the input dataset+normals to it
+
+		pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> vfh;
+		vfh.setInputCloud(cloud_cluster);
+		vfh.setInputNormals(normals);
+
+		// alternatively, if cloud is of tpe PointNormal, do vfh.setInputNormals (cloud);
+
+		// Create an empty kdtree representation, and pass it to the FPFH estimation object.
+		// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+		pcl::KdTreeFLANN<pcl::PointXYZ>::Ptr tree (new pcl::KdTreeFLANN<pcl::PointXYZ> ());
+		//vfh.setSearchMethod (tree);
+
+
+		// Output datasets
+		pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs (new pcl::PointCloud<pcl::VFHSignature308> ());
+
+		// Compute the features
+		vfh.compute (*vfhs);
+
+		ss.clear();
+		ss << "cloud_cluster_" << j << "vfh" << ".pcd";
+		writer.write<pcl::VFHSignature308> (ss.str (), *vfhs, false);
+	 }
+
+	writer.write<pcl::PointXYZ> ( "scence.pcd" , *cloud_filtered , false );
+	// ... do data processing
+	exit(0);
+}
+
+int main (int argc, char** argv)
+{
+	// Initialize ROS
+	ros::init (argc, argv, "my_pcl_tutorial");
+	ros::NodeHandle nh;
+	// Create a ROS subscriber for the input point cloud
+	ros::Subscriber sub = nh.subscribe ("/camera/depth/points", 1, cloud_cb);
+
+	// Create a ROS publisher for the output point cloud
+	pub = nh.advertise<sensor_msgs::PointCloud2> ("input", 1);
+
+	// Spin
+	ros::spin ();
+}
