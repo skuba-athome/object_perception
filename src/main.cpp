6ecfb084f1a89c5582880a9878550aa1d8498c7a
pond <pond@localhost>
2012-04-27 00:00:00 +0700

    Modify objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,140 @@
-#include "main.h"
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/print.h>
+#include <pcl/io/pcd_io.h>
+#include <boost/filesystem.hpp>
+#include <flann/flann.h>
+#include <flann/io/hdf5.h>
+#include <fstream>
 
-void currnetFrame_mouse_callback( int event, int x, int y, int flags, void* param ){
-	switch( event ){
-		case CV_EVENT_LBUTTONDOWN :
-			min_x = x;
-			min_y =y;
-			break;
-		case CV_EVENT_RBUTTONDOWN :
-			max_x = x;
-			max_y = y;
-			break;
-	}
-	//cvRectangle(CvArr* img, CvPoint pt1, CvPoint pt2, CvScalar color, int thickness=1, int lineType=8, int shift=0)
+typedef std::pair<std::string, std::vector<float> > vfh_model;
+
+/** \brief Loads an n-D histogram file as a VFH signature
+  * \param path the input file name
+  * \param vfh the resultant VFH model
+  */
+bool
+loadHist (const boost::filesystem::path &path, vfh_model &vfh)
+{
+  int vfh_idx;
+  // Load the file as a PCD
+  try
+  {
+    sensor_msgs::PointCloud2 cloud;
+    int version;
+    Eigen::Vector4f origin;
+    Eigen::Quaternionf orientation;
+    pcl::PCDReader r;
+    //int type; unsigned int idx;
+    //r.readHeader (path.string (), cloud, origin, orientation, version, type, idx);
+    int type; int idx;
+    const std::string temp_str = path.string();
+    r.readHeader(temp_str, cloud, origin, orientation, version, type, idx);
+
+    vfh_idx = pcl::getFieldIndex (cloud, "vfh");
+    if (vfh_idx == -1)
+      return (false);
+    if ((int)cloud.width * cloud.height != 1)
+      return (false);
+  }
+  catch (pcl::InvalidConversionException e)
+  {
+    return (false);
+  }
+
+  // Treat the VFH signature as a single Point Cloud
+  pcl::PointCloud <pcl::VFHSignature308> point;
+  pcl::io::loadPCDFile (path.string (), point);
+  vfh.second.resize (308);
+
+  std::vector <sensor_msgs::PointField> fields;
+  pcl::getFieldIndex (point, "vfh", fields);
+
+  for (size_t i = 0; i < fields[vfh_idx].count; ++i)
+  {
+    vfh.second[i] = point.points[0].histogram[i];
+  }
+  vfh.first = path.string ();
+  return (true);
 }
 
-void depthCb( const sensor_msgs::ImageConstPtr& image )
+/** \brief Load a set of VFH features that will act as the model (training data)
+  * \param argc the number of arguments (pass from main ())
+  * \param argv the actual command line arguments (pass from main ())
+  * \param extension the file extension containing the VFH features
+  * \param models the resultant vector of histogram models
+  */
+void
+loadFeatureModels (const boost::filesystem::path &base_dir, const std::string &extension,
+                   std::vector<vfh_model> &models)
 {
-	canUseDist = 0;
-    try
+  if (!boost::filesystem::exists (base_dir) && !boost::filesystem::is_directory (base_dir))
+    return;
+
+  for (boost::filesystem::directory_iterator it (base_dir); it != boost::filesystem::directory_iterator (); ++it)
+  {
+    if (boost::filesystem::is_directory (it->status ()))
     {
-        bridge = cv_bridge::toCvCopy(image, "32FC1");
+      std::stringstream ss;
+      ss << it->path ();
+      pcl::console::print_highlight ("Loading %s (%lu models loaded so far).\n", ss.str ().c_str (), (unsigned long)models.size ());
+      loadFeatureModels (it->path (), extension, models);
     }
-    catch (cv_bridge::Exception& e)
+    if (boost::filesystem::is_regular_file (it->status ()) && boost::filesystem::extension (it->path ()) == extension)
     {
-        ROS_ERROR("Failed to transform depth image.");
-        return;
+      vfh_model m;
+      if (loadHist (base_dir / it->path ().filename (), m))
+        models.push_back (m);
     }
-    depthImg = Mat(bridge->image.rows, bridge->image.cols, CV_8UC1);
-    for(int i = 0; i < bridge->image.rows; i++)
-    {
-        float* Di = bridge->image.ptr<float>(i);
-        char* Ii = depthImg.ptr<char>(i);
-        for(int j = 0; j < bridge->image.cols; j++)
-        {
-            Ii[j] = (char) (255*((Di[j]-min_range_)/(max_range_-min_range_)));
-            dist[i][j] = Di[j];
-        }
-    }
-    canUseDist = 1;
+  }
 }
 
-void imageCb(const sensor_msgs::ImageConstPtr& msg)
+int
+main (int argc, char** argv)
 {
-	IpVec ipts , ipts2 ;
-	IpPairVec matches;
+  if (argc < 2)
+  {
+    PCL_ERROR ("Need at least two parameters! Syntax is: %s [model_directory] [options]\n", argv[0]);
+    return (-1);
+  }
 
-	for(int i=0;i<WIDTH*HIEGHT;i++)
-	{
-		currnetFrame->imageData[i] = msg->data[i];
-	}
+  std::string extension (".pcd");
+  transform (extension.begin (), extension.end (), extension.begin (), (int(*)(int))tolower);
 
-	//cvSmooth(currnetFrame,currnetFrame,CV_GAUSSIAN);
-	cvRectangle(currnetFrame,cvPoint(min_x,min_y),cvPoint(max_x,max_y), cvScalarAll(7.0) );
-	surfDetDes(currnetFrame, ipts, false, 3, 4, 3, 0.004f); // Detect and describe interest points in the frame
-	IplImage *tmp = cvLoadImage("./imgs/02.pgm",CV_LOAD_IMAGE_GRAYSCALE);
-	surfDetDes(tmp, ipts2, false, 3, 4, 3, 0.004f); // Detect and describe interest points in the frame
-	for(int i = 0 ; i< ipts.size();i++)
-	{
-		float x = ipts[i].x;
-		float y = ipts[i].y;
+  std::string kdtree_idx_file_name = "kdtree.idx";
+  std::string training_data_h5_file_name = "training_data.h5";
+  std::string training_data_list_file_name = "training_data.list";
 
-		if( dist[ (int)(y/HIEGHT*480) ][ (int)(x/WIDTH*640) ] < 2.0f )
-		{
-			ipts.erase(ipts.begin()+i);
-			i--;
-		}
-		printf("%d %d %.2f\n",(int)(y/HIEGHT*480),(int)(x/WIDTH*640),dist[ (int)(y/HIEGHT*480) ][ (int)(x/WIDTH*640) ]);
-	}
-	printf("-------------------------------------------");
-	getMatches(ipts,ipts2,matches); // Fill match vector
+  std::vector<vfh_model> models;
 
-    for (unsigned int i = 0; i < matches.size(); i++)
-    {
-      const float & dx = matches[i].first.dx;
-      const float & dy = matches[i].first.dy;
-      if(canUseDist)// if( dist[ (int)(y/HIEGHT*480) ][ (int)(x/WIDTH*640) ] < 2.0f )
-		  drawIpoint(currnetFrame , matches[i].first, 3);
-    }
+  // Load the model histograms
+  loadFeatureModels (argv[1], extension, models);
+  pcl::console::print_highlight ("Loaded %d VFH models. Creating training data %s/%s.\n",
+      (int)models.size (), training_data_h5_file_name.c_str (), training_data_list_file_name.c_str ());
 
-	// Display image
-	cvShowImage("currnetFrame",currnetFrame);
-	cvShowImage("tmp",tmp);
-	char inKey = cvWaitKey(1);
-	if(inKey == 27){
-		exit(0);
-	}
+  // Convert data into FLANN format
+  flann::Matrix<float> data (new float[models.size () * models[0].second.size ()], models.size (), models[0].second.size ());
 
-	cvReleaseImage(&tmp);
-}
+  for (size_t i = 0; i < data.rows; ++i)
+    for (size_t j = 0; j < data.cols; ++j)
+      data[i][j] = models[i].second[j];
 
+  // Save data to disk (list of models)
+  flann::save_to_file (data, training_data_h5_file_name, "training_data");
+  std::ofstream fs;
+  fs.open (training_data_list_file_name.c_str ());
+  for (size_t i = 0; i < models.size (); ++i)
+    fs << models[i].first << "\n";
+  fs.close ();
 
-int main(int argc,char * argv[])
-{
-	ros::init(argc,argv,"capture");
-	ros::NodeHandle n;
-	ros::NodeHandle nh("~");
-	nh.param("min_range", min_range_, 0.5);
-	nh.param("max_range", max_range_, 5.5);
-	ros::Subscriber sub = n.subscribe("/camera/rgb/image_mono",1, imageCb);
-	ros::Subscriber sub2 = nh.subscribe("/camera/depth/image",1, depthCb);
+  // Build the tree index and save it to disk
+  pcl::console::print_error ("Building the kdtree index (%s) for %d elements...\n", kdtree_idx_file_name.c_str (), (int)data.rows);
+  flann::Index<flann::ChiSquareDistance<float> > index (data, flann::LinearIndexParams ());
+  //flann::Index<flann::ChiSquareDistance<float> > index (data, flann::KDTreeIndexParams (4));
+  index.buildIndex ();
+  index.save (kdtree_idx_file_name);
+  delete[] data.ptr ();
 
-	cvNamedWindow( "currnetFrame" );
-
-	// Set up the callback
-	cvSetMouseCallback( "currnetFrame", currnetFrame_mouse_callback );
-
-
-	ros::spin();
+  return (0);
 }
