58de16d37e6c14839b136212fb994bb38a61093c
Mukda T <mukmemail@gmail.com>
2015-05-01 09:00:51 +0900

    change type of object position and change path dir

---
+++ object_recognition/nodes/verify_object_service.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+from sklearn import neighbors, datasets
+import roslib
+import os
+roslib.load_manifest('object_recognition')
+
+import rospy
+import cv2
+
+from std_msgs.msg import String
+from object_recognition.srv import *
+from operator import add
+
+
+surf = cv2.SURF(400)
+
+class object_recognition:
+
+    def __init__(self):
+        rospy.init_node('verify_object_service')
+        rospy.Subscriber("/object_recognition/create_model", String, self.create_model)
+        
+        feature_directory = rospy.get_param('~feature_directory', '/config/feature')
+        K_neighbors = int(rospy.get_param('~k_neighbors', 35))
+
+
+        print feature_directory
+        feature, self.labels = self.load_train_data(feature_directory)
+        self.clf = neighbors.KNeighborsClassifier(K_neighbors, weights='distance')
+        self.clf.fit(feature, self.labels) 
+
+        object_threshold_file = rospy.get_param('~threshold_file', 'config/threshold.txt')
+        #object_threshold_file = "/home/skuba/skuba_athome/object_perception/object_recognition/config/result_threshold.txt"
+        self.object_threshold = self.read_object_threshold(object_threshold_file)
+        print object_threshold_file
+        print self.object_threshold
+        
+        rospy.Service('/object_recognition/verify_object', classifyObject, self.classify_object_service)
+        
+        rospy.loginfo('Verify node Start')
+        rospy.spin()
+		
+    def read_object_threshold(self,file_name):
+        file_ptr = open(file_name,"r")
+        threshold_list = {}
+        for line in file_ptr:
+            object_name, threshold = line.split(':')
+            threshold_list[object_name] = float(threshold)
+        threshold_list['unknown'] = 1.0
+        return threshold_list
+
+    def load_train_data(self, feature_directory):
+        feature_list = []
+        label_list = []
+        self.category_set = []
+        for feature_file in os.listdir(feature_directory):
+            if feature_file.endswith(".txt"):
+                category = os.path.splitext(os.path.basename(feature_file))[0]
+                self.category_set.append(category)
+                category_index = self.category_set.index(category)
+                feature, label = self.load_feature(os.path.join(feature_directory,feature_file), category_index)
+                
+                feature_list += feature
+                label_list += label
+                
+        self.category_set.append('unknown')
+        return feature_list,label_list
+
+    def load_feature(self, filename, category):
+        feature_list = []
+        label_list = []
+        file_feature = open(filename,"r")
+        for line in file_feature:
+            attribute = line.strip().split()
+            feature_list.append(map(float,attribute))
+            label_list.append(int(category))
+        file_feature.close()
+        return feature_list,label_list
+    
+    def create_model(self,data):
+        print 'in create_model'
+        feature,self.labels = self.loadTrainData(data.data)
+        self.clf = neighbors.KNeighborsClassifier(K_neighbors, weights='distance')
+        self.clf.fit(feature, self.labels) 
+
+    def classify_object_service(self, req):
+        print "incoming input :", req.filepath
+        category, confident = self.predict_object(req.filepath)
+        #return classifyObjectResponse(str(self.category_set[category]),confident)
+        if self.object_threshold[str(self.category_set[category])] > confident:
+            return classifyObjectResponse(str(self.category_set[category]),confident)
+        print 'predicted result(wrong) as',str(self.category_set[category]),'with confident',confident
+        return classifyObjectResponse('unknown',confident)
+    
+    def predict_object(self, image_filename):
+        image = cv2.imread(image_filename, 0)
+        keypoint, features = surf.detectAndCompute(image, None)
+
+        if features == None or len(keypoint) == 0:
+            return -1, 0.0
+
+        query_feature = [map(float,feature) for feature in features]
+
+        weight_sum = [0.0 for i in self.category_set]
+        for feature in query_feature:
+            weight = self.predict_feature(feature)
+            weight_sum = map(add, weight, weight_sum)
+            
+        sortedWeightSum = sorted(weight_sum)
+        print "filename :", image_filename , "with difference :",str(sortedWeightSum[0]-sortedWeightSum[1])
+        return int(weight_sum.index(min(weight_sum))), (sortedWeightSum[0]-sortedWeightSum[1])
+    
+    def predict_feature(self, feature):
+        class_set = []
+        weight = [0.0 for i in self.category_set]
+        dis,ind = self.clf.kneighbors(feature)
+        dis = dis[0]
+        ind = ind[0]
+        for x in range(len(ind)):
+            if(self.labels[ind[x]] in class_set):
+                continue
+            class_set.append(self.labels[ind[x]])
+            weight[self.labels[ind[x]]] = dis[x] - dis[-1]
+        return weight
+ 
+if __name__ == '__main__':
+    try:
+        rospy.loginfo('Verification Node Start Initializing')
+        object_recognition()
+    except rospy.ROSInterruptException:
+        rospy.logerror('Error ROS')
+
---
+++ object_recognition/src/click_object.cpp
@@ -0,0 +1,211 @@
+/*
+ * Description : Do(and read) index using flann & surf
+ * Author      : Chanon Onman
+ */
+#include <cv.h>
+#include <highgui.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include <iostream>
+#include <vector>
+
+//
+#include<ros/ros.h>
+#include<sensor_msgs/Image.h>
+#include<std_msgs/String.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <geometry_msgs/Vector3.h>
+//---pcl---
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+// PCL specific includes
+//#include <pcl/ros/conversions.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+
+
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/features/vfh.h>
+
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+#include <tf/transform_listener.h>
+#include <pcl_ros/transforms.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+
+//---------
+using namespace std;
+using namespace cv;
+
+//std::string rgb_frame = "/camera_rgb_frame";
+ros::Time timeStamp;
+std::string rgb_frame = "/camera_rgb_optical_frame";
+std::string robot_frame = "/base_link";
+tf::TransformListener* listener;
+ros::Publisher vector_pub; // = n2.advertise<geometry_msgs::Vector3>("object_point", 1000);
+ros::Publisher vector_pub_pour; 
+ros::Publisher vector_pub_pointcloud;
+IplImage *inFrame  = cvCreateImage(cvSize(640, 480), 8, 3);
+static pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_pcl (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+void depthCb(const sensor_msgs::PointCloud2& cloud) {
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_tmp (new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PCLPointCloud2 PCLPointCloud_tmp;
+  if ((cloud.width * cloud.height) == 0)
+    return; //return if the cloud is not dense!
+  try {
+    pcl_conversions::toPCL(cloud, PCLPointCloud_tmp);
+
+    //pcl::fromROSMsg(cloud, *cloud_tmp);
+
+    pcl::fromPCLPointCloud2(PCLPointCloud_tmp, *cloud_tmp);
+
+    //cloud_tmp->header = cloud.header;
+
+    timeStamp = cloud.header.stamp;
+    listener->waitForTransform(rgb_frame, cloud.header.frame_id, cloud.header.stamp, ros::Duration(1.0));
+    pcl_ros::transformPointCloud(rgb_frame, *cloud_tmp, *cloud_pcl, *listener);
+
+  } catch (std::runtime_error e) {
+    ROS_ERROR_STREAM("Error message: " << e.what());
+  }
+}
+
+
+// mouse callback
+void on_mouse( int event, int x, int y, int flags, void* param )
+{
+	if(event == CV_EVENT_LBUTTONUP) 
+	{
+		ROS_INFO("click_at : x:%d y:%d",x,y);
+		geometry_msgs::Vector3 vector;
+		vector.x = cloud_pcl->points[y*640+x].x;
+		vector.y = cloud_pcl->points[y*640+x].y;
+		vector.z = cloud_pcl->points[y*640+x].z;
+		if( vector.x == vector.x 
+			&& vector.y == vector.y
+			&& vector.z == vector.z
+		)
+		{
+
+            geometry_msgs::PointStamped kinect_point;
+            geometry_msgs::PointStamped base_point;
+
+            kinect_point.header.frame_id = rgb_frame;
+            kinect_point.header.stamp = timeStamp;
+            kinect_point.point.x = vector.x;
+            kinect_point.point.y = vector.y;
+            kinect_point.point.z = vector.z;
+            listener->transformPoint(robot_frame,kinect_point,base_point);
+            vector.x = base_point.point.x;
+            vector.y = base_point.point.y;
+            vector.z = base_point.point.z;
+
+//            sensor_msgs::PointCloud2 cloud_tf_out;
+//            pcl::toROSMsg(*cloud_pcl,cloud_tf_out);
+            //vector_pub_pointcloud.publish(cloud_tf_out);
+            vector_pub.publish(vector);
+            printf("send : x:%.5f y:%.5f z:%.5f\n",vector.x,vector.y,vector.z);						
+		}
+
+	}
+	if(event == CV_EVENT_RBUTTONUP) 
+	{
+        ROS_INFO("R_click");
+		ROS_INFO("click_at : x:%d y:%d",x,y);
+		geometry_msgs::Vector3 vector;
+		vector.x = cloud_pcl->points[y*640+x].x;
+		vector.y = cloud_pcl->points[y*640+x].y;
+		vector.z = cloud_pcl->points[y*640+x].z;
+		if( vector.x == vector.x 
+			&& vector.y == vector.y
+			&& vector.z == vector.z
+		)
+		{
+
+            geometry_msgs::PointStamped kinect_point;
+            geometry_msgs::PointStamped base_point;
+
+            kinect_point.header.frame_id = rgb_frame;
+            kinect_point.header.stamp = timeStamp;
+            kinect_point.point.x = vector.x;
+            kinect_point.point.y = vector.y;
+            kinect_point.point.z = vector.z;
+            listener->transformPoint(robot_frame,kinect_point,base_point);
+            vector.x = base_point.point.x;
+            vector.y = base_point.point.y;
+            vector.z = base_point.point.z;
+
+//            sensor_msgs::PointCloud2 cloud_tf_out;
+//            pcl::toROSMsg(*cloud_pcl,cloud_tf_out);
+            //vector_pub_pointcloud.publish(cloud_tf_out);
+            vector_pub_pour.publish(vector);
+            printf("send : x:%.5f y:%.5f z:%.5f\n",vector.x,vector.y,vector.z);						
+		}
+
+	}
+}
+
+void kinectCallBack(const sensor_msgs::ImageConstPtr& msg)
+{
+        int inKey = 0;
+
+	for(int i=0;i<640*480;i++)
+	{
+			inFrame->imageData[i*3] = msg->data[i*3];
+			inFrame->imageData[i*3+1] = msg->data[i*3+1];
+			inFrame->imageData[i*3+2] = msg->data[i*3+2];
+	}
+		
+	cvShowImage("input",inFrame);
+
+	inKey = cvWaitKey(1);
+	if(inKey == 27){
+		exit(0);
+	}
+}
+
+
+int main(int argc , char *argv[])
+{
+	ros::init(argc,argv,"click_objects");
+	ros::NodeHandle n;
+	ros::NodeHandle nh("~");
+	
+	ros::Subscriber sub = n.subscribe("/camera/rgb/image_color",1,kinectCallBack);
+	//ros::Subscriber subDepth = n.subscribe("/cloud_tf",1,depthCb);
+	ros::Subscriber subDepth = n.subscribe("/camera/depth_registered/points",1,depthCb);
+	
+	//vector_pub = n.advertise<geometry_msgs::Vector3>("object_point", 1000);
+	vector_pub = n.advertise<geometry_msgs::Vector3>("manipulator/object_point_split", 1000);
+	//vector_pub = n.advertise<geometry_msgs::Vector3>("manipulator/grasp", 1000);
+	vector_pub_pour = n.advertise<geometry_msgs::Vector3>("manipulator/pour", 1000);
+	vector_pub_pointcloud = n.advertise<sensor_msgs::PointCloud2>("object_pointcloud", 1000);
+
+    listener = new tf::TransformListener();
+	printf("start click_objects\n");
+	cvNamedWindow("input", 1 );
+	cvSetMouseCallback("input", on_mouse);
+	ros::spin();
+}
