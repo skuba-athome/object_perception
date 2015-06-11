ca499343557ac83b4cd74492befbccf63d8da7d0
mukda <mukda@localhost>
2015-06-11 23:54:58 +0700

    add object and table shape detection

---
+++ object_detection/src/object_shapes.cpp
@@ -0,0 +1,163 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include "std_msgs/String.h"
+#include <object_recognition_msgs/RecognizedObjectArray.h>
+//#include <shape_msgs/SolidPrimitive.h>
+#include <object_detection/ObjectDetection.h>
+
+
+class ObjectShape
+{
+public:
+  ObjectShape()
+  {
+    object_sub = nh.subscribe("recognized_object_array", 1, &ObjectShape::objectCallback, this);
+    marker_pub = nh.advertise<visualization_msgs::Marker>("object_marker", 1);
+    solid_shape_pub = nh.advertise<object_detection::ObjectDetection>("object_shape", 1);
+    //solid_shape_pub = nh.advertise<shape_msgs::SolidPrimitive>("object_shape", 1);
+    //solid_shape_serv = nh.advertiseService("object_shape", &ObjectShape::getSolidPrimitive, this);
+  }
+
+
+  void objectCallback(const object_recognition_msgs::RecognizedObjectArrayPtr& object_array)
+  {
+    //ROS_INFO("objectCallback");
+    
+    for (size_t index = 0; index < 1; ++index) 
+    {
+        object_recognition_msgs::RecognizedObject object = object_array->objects[index];
+        std_msgs::Header object_header = object.header;
+        //ROS_INFO("  header %s %f ", object_header.frame_id.c_str(), (double)object_header.seq);        
+
+        object_pose = object.pose.pose.pose;
+        getCloudMarker(object_pose);
+        getSolidPrimitive();
+        
+
+      /*
+      object_recognition_msgs/ObjectType type
+      float32 confidence
+
+      sensor_msgs/PointCloud2[] point_clouds
+      shape_msgs/Mesh bounding_mesh
+      geometry_msgs/Point[] bounding_contours
+
+      geometry_msgs/PoseWithCovarianceStamped pose
+      */
+
+    }
+  }
+
+  void getCloudMarker(const geometry_msgs::Pose object_pose)
+  {
+      //ROS_INFO("getCloudMarker");
+      visualization_msgs::Marker marker;
+
+      marker.header.frame_id = "/camera_depth_optical_frame";
+      marker.action = visualization_msgs::Marker::ADD;
+      marker.header.stamp = ros::Time();
+
+      marker.type = visualization_msgs::Marker::CUBE;
+
+      marker.pose.position.x = object_pose.position.x + 0.05;
+      marker.pose.position.y = object_pose.position.y - 0.05;
+      marker.pose.position.z = object_pose.position.z - 0.1;
+      marker.pose.orientation.x = object_pose.orientation.x;
+      marker.pose.orientation.y = object_pose.orientation.y;
+      marker.pose.orientation.z = object_pose.orientation.z;
+      marker.pose.orientation.w = object_pose.orientation.w;
+      
+      marker.scale.x = 0.05;
+      marker.scale.y = 0.05;
+      marker.scale.z = 0.1;
+
+      marker.color.r = 1.0f;
+      marker.color.g = 0.0f;
+      marker.color.b = 0.0f;
+      marker.color.a = 1.0; 
+      
+
+      /*
+      marker.type = visualization_msgs::Marker::POINTS;
+      
+      marker.scale.x = 0.02;
+      marker.scale.y = 0.02;
+      marker.scale.z = 0.1;
+
+      marker.color.r = 1.0f;
+      marker.color.g = 0.0f;
+      marker.color.b = 0.0f;
+      marker.color.a = 1.0;
+
+      geometry_msgs::Point point;
+      point.x = object_pose.position.x;
+      point.y = object_pose.position.y;
+      point.z = object_pose.position.z;
+      marker.points.push_back(point);
+
+      /*
+      for (size_t i = 0; i < object_contours.size(); i++) 
+      {
+          geometry_msgs::Point point;
+          point.x = object_contours[i].x;
+          point.y = object_contours[i].y;
+          point.z = object_contours[i].z;
+          marker.points.push_back(point);
+      }
+      ROS_INFO("object_contours %d",(int)object_contours.size());
+      */
+      marker_pub.publish(marker);
+
+  }
+
+  //bool getSolidPrimitive(using_markers::ObjectShape::Request &req, using_markers::ObjectShape::Response &res)
+  void getSolidPrimitive()
+  {
+    ROS_INFO("Object Primitive Shape");
+    shape_msgs::SolidPrimitive solid_shape;
+    solid_shape.type = shape_msgs::SolidPrimitive::BOX;
+    solid_shape.dimensions.resize(3);
+    //solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_X] = 0.3;
+    //solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 0.3;
+    //solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.03;
+    solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_X] = 0.05;
+    solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 0.05;
+    solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.1;
+    
+    //solid_shape.dimensions.push_back(0.3);
+    //solid_shape.dimensions.push_back(0.3);
+    //solid_shape.dimensions.push_back(0.3);
+    //res.solid_shape = solid_shape;
+    //res.centriod = object_pose;
+    
+    //solid_shape_serv.publish(solid_shape);
+    //solid_shape_pub.publish(solid_shape);
+    object_detection::ObjectDetection msg;
+    msg.solid_shape = solid_shape;
+    msg.centriod = object_pose.position;
+    solid_shape_pub.publish(msg);
+
+    //ROS_INFO("Table Plane Primitive Shape: [%d %.2f %.2f]", (int)res.solid_shape.type, size_x, size_y);
+    //return true;
+  }
+
+
+private:
+  ros::NodeHandle nh; 
+  ros::Subscriber object_sub;
+  object_recognition_msgs::RecognizedObjectArray object_array;
+  ros::Publisher marker_pub;
+  //ros::ServiceServer solid_shape_serv;
+  ros::Publisher solid_shape_pub;
+  geometry_msgs::Pose object_pose;
+};
+
+// %Tag(INIT)%
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "object_shapes");
+  ROS_INFO("object_shapes starting ... ^^");
+  ObjectShape object_shape;
+  ros::spin();
+  return 0;
+}
---
+++ object_detection/src/table_shapes.cpp
@@ -0,0 +1,188 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include "std_msgs/String.h"
+#include <object_recognition_msgs/TableArray.h>
+#include <shape_msgs/SolidPrimitive.h>
+//#include "using_markers/TableShape.h"
+#include <object_detection/ObjectDetection.h>
+
+class TableShape
+{
+public:
+  TableShape()
+  {
+    table_sub = nh.subscribe("table_array", 1, &TableShape::tableCallback, this);
+    marker_pub = nh.advertise<visualization_msgs::Marker>("table_marker", 1);
+    solid_shape_pub = nh.advertise<object_detection::ObjectDetection>("table_shape", 1);
+    //solid_shape_pub = nh.advertise<shape_msgs::SolidPrimitive>("table_shape", 1);
+    //solid_shape_serv = nh.advertiseService("table_shape", &TableShape::getSolidPrimitive, this);
+  }
+
+  void tableCallback(const object_recognition_msgs::TableArrayPtr& table_array)
+  {
+    //ROS_INFO("tableCallback");
+    
+    for (size_t index = 0; index < 1; ++index) 
+    //for (std::vector<int>::iterator it = table_array.begin(); table_array.end(); it++)
+    {
+        object_recognition_msgs::Table table = table_array->tables[index];
+        std_msgs::Header table_header = table.header;
+        //geometry_msgs::Pose table_pose = table.pose;
+        table_pose = table.pose;
+        //ROS_INFO("POSE: [%f %f %f]", table_pose.position.x, table_pose.position.y, table_pose.position.z);
+        
+        std::vector<geometry_msgs::Point> table_convex_hull = table.convex_hull;
+        //ROS_INFO("  table_convex_hull %d",(int)table_convex_hull.size());
+
+        double xmax = 2.2250738585072014e-308;
+        double xmin = 1.7976931348623158e+308;
+        double ymax = 2.2250738585072014e-308;
+        double ymin = 1.7976931348623158e+308;
+        double z = 0;
+
+
+        for (size_t i = 0; i < table_convex_hull.size(); i++) 
+        {
+          geometry_msgs::Point vertex;
+          vertex.x = table_convex_hull[i].x;
+          vertex.y = table_convex_hull[i].y;
+          vertex.z = table_convex_hull[i].z;
+
+          if(xmin>=vertex.x)
+            xmin = vertex.x;
+          if(ymin>=vertex.y)
+            ymin = vertex.y;
+          if(xmax<=vertex.x)
+            xmax = vertex.x;
+          if(ymax<=vertex.y)
+            ymax = vertex.y;
+          z = vertex.z;
+
+          //ROS_INFO(" vertex: [%f %f %f]", vertex.x, vertex.y, vertex.z);  
+        }
+        //ROS_INFO(" min_max: [%f %f %f %f %f]", xmin, xmax, ymin, ymax, z);
+
+        //double size_x = xmax-xmin;
+        //double size_y = ymax-ymin;
+        size_x = xmax-xmin;
+        size_y = ymax-ymin;
+        //ROS_INFO(" size: [%f %f]", xmax-xmin, ymax-ymin);
+        //object_recognition_msgs::Table table = table_array->tables[*it];
+        //visualization_msgs::Marker marker = getCloudMarker(table);
+        //marker_table = getCloudMarker(table);
+
+        //getCloudMarker(table_pose, size_x, size_y);
+        getCloudMarker(table);
+        getSolidPrimitive();
+    }
+  }
+
+  void getCloudMarker(const object_recognition_msgs::Table table)
+  //void getCloudMarker(const geometry_msgs::Pose table_pose, const double size_x, const double size_y)
+  {
+      //ROS_INFO("getCloudMarker");
+      visualization_msgs::Marker marker;
+
+      marker.header.frame_id = "/camera_depth_optical_frame";
+      marker.action = visualization_msgs::Marker::ADD;
+      marker.header.stamp = ros::Time();
+      //marker.lifetimer = ros::Duration(5);
+      
+      marker.type = visualization_msgs::Marker::CUBE;
+
+      marker.pose.position.x = table_pose.position.x;
+      marker.pose.position.y = table_pose.position.y;
+      marker.pose.position.z = table_pose.position.z;
+      marker.pose.orientation.x = table_pose.orientation.x;
+      marker.pose.orientation.y = table_pose.orientation.y;
+      marker.pose.orientation.z = table_pose.orientation.z;
+      marker.pose.orientation.w = table_pose.orientation.w;
+      
+      marker.scale.x = size_x;
+      marker.scale.y = size_y;
+      marker.scale.z = 0.03;
+
+      marker.color.r = 0.0f;
+      marker.color.g = 0.0f;
+      marker.color.b = 1.0f;
+      marker.color.a = 1.0;
+      
+      /*
+      marker.type = visualization_msgs::Marker::POINTS;
+      marker.scale.x = 0.02;
+      marker.scale.y = 0.02;
+      marker.scale.z = 0.1;
+
+      marker.color.r = 1.0f;
+      marker.color.g = 0.0f;
+      marker.color.b = 0.0f;
+      marker.color.a = 1.0;
+
+      for (size_t i = 0; i < table.convex_hull.size(); i++) 
+      {
+          geometry_msgs::Point point;
+          point.x = table.convex_hull[i].x;
+          point.y = table.convex_hull[i].y;
+          point.z = table.convex_hull[i].z;
+          marker.points.push_back(point);
+      }
+      //ROS_INFO("table.convex_hull %d",(int)table.convex_hull.size());
+      */
+      marker_pub.publish(marker);
+      //return marker;
+  }
+
+  //bool getSolidPrimitive(using_markers::TableShape::Request &req, using_markers::TableShape::Response &res)
+  void getSolidPrimitive()
+  {
+    ROS_INFO("Table Plane Primitive Shape");
+
+    shape_msgs::SolidPrimitive solid_shape;
+    solid_shape.type = shape_msgs::SolidPrimitive::BOX;
+    solid_shape.dimensions.resize(3);
+    //solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_X] = 0.3;
+    //solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 0.3;
+    //solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.03;
+    solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_X] = size_x;
+    solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Y] = size_y;
+    solid_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.03;
+    
+    //solid_shape.dimensions.push_back(0.3);
+    //solid_shape.dimensions.push_back(0.3);
+    //solid_shape.dimensions.push_back(0.3);
+    //res.solid_shape = solid_shape;
+    //res.centriod = table_pose;
+    
+    object_detection::ObjectDetection msg;
+    msg.solid_shape = solid_shape;
+    msg.centriod = table_pose.position;
+    solid_shape_pub.publish(msg);
+
+    //solid_shape_pub.publish(solid_shape);
+
+    //ROS_INFO("Table Plane Primitive Shape: [%d %.2f %.2f]", (int)res.solid_shape.type, size_x, size_y);
+    //return true;
+  }
+
+private:
+  ros::NodeHandle nh; 
+  ros::Subscriber table_sub;
+  ros::Publisher marker_pub;
+  //ros::ServiceServer solid_shape_serv;
+  ros::Publisher solid_shape_pub;
+  object_recognition_msgs::TableArray table_array;
+  geometry_msgs::Pose table_pose;
+  double size_x;
+  double size_y;
+};
+
+
+// %Tag(INIT)%
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "table_shapes");
+  ROS_INFO("table_shapes starting ... ^^");
+  TableShape table_shape;
+  ros::spin();
+  return 0;
+}
