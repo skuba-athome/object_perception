d9f6c6af83fd2647ddeb454156dcf03738390a6c
gun <gun@localhost>
2016-03-05 01:24:40 +0700

    update ClothesDetector Component(EGBIS Localization)

--- clothing_type_classification/include/ClothesDetector.h
+++ clothing_type_classification/include/ClothesDetector.h
@@ -21,17 +21,47 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "egbis.h"
 
+
 class ClothesDetector
 {
     public:
-        ClothesDetector();
-        void findSegmentBoundary();
-        void detectClothesObjects();
-        void getBinaryImage(std::vector<cv::Mat>& images, std::vector<cv::Mat>& image_th, int threshold_value);
 
+        class DetectorDescriptors
+        {
+            public:
+                DetectorDescriptors();
+                std::vector<cv::Point> contour;
+                double contour_area;
+                cv::Point2f centroid;
+                cv::Rect rect;
+                cv::Mat mask;
+                cv::Mat cropped;
+                uint8_t type;
+                void copyTo(DetectorDescriptors &target);
+                inline void operator = ( DetectorDescriptors &target );
+        };
+        enum
+        {
+                TABLE,
+                CLOTHES,
+                UNKNOWN
+        };
+
+        ClothesDetector();
+        //Morphological Kernel MORPH_RECT,MORPH_CROSS,MORPH_ELLIPSE
+        void setOriginalImage(cv::Mat img);
+        void getBinaryImage(std::vector<cv::Mat>& images, std::vector<cv::Mat>& image_th, int threshold_value,
+                            int closing_window_size = 5, int opening_window_size = 5, int kernel_type = MORPH_ELLIPSE );
+
+        void detectClothesObjects(std::vector<cv::Mat> &images_th, std::vector<DetectorDescriptors>& out, bool crop_original = true);
+        void saveOutputImages(std::vector<DetectorDescriptors>& images, std::string filename = "out",
+                              bool draw_descriptors = true);
 
     private:
-
+        cv::Mat original;
+        void computeDescriptors(cv::Mat images_th, DetectorDescriptors &out);
+        void drawDescriptors(DetectorDescriptors& input, cv::Mat& output);
+        void cropOriginal(std::vector<DetectorDescriptors>& out);
 };
 
 
